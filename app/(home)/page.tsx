2260d94f6dbb90348431928da2f825dfbd4adb8f
mohammad taghinasab <mohammad.sawdegh@gmail.com>
2024-03-28 18:21:51 +0330

    feat: fix folders

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="flex size-full items-center justify-center">Welcome To Nerd Studio</main>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./styles/globals.css";
 import Sidebar from "./components/shared/Sidebar";
 import Navbar from "./components/shared/Navbar";
 
