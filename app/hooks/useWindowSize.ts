55fcef0a4e3525452435a5461c3b43d709486006
mohammad taghinasab <mohammad.sawdegh@gmail.com>
2024-03-30 12:50:24 +0330

    fix: fix costum hook build error in another way

--- app/hooks/useWindowSize.ts
+++ app/hooks/useWindowSize.ts
@@ -1,15 +1,18 @@
-import { useLayoutEffect, useState } from 'react';
+import { useEffect, useLayoutEffect, useState } from "react";
 
 const useWindowSize = () => {
-  const isClient = typeof window === 'object';
-  const [windowSize, setWindowSize] = useState(isClient ? window.innerWidth : undefined);
+  const [windowSize, setWindowSize] = useState(0);
+
+  useEffect(() => {
+    setWindowSize(window.innerWidth);
+  },[])
 
   useLayoutEffect(() => {
     const updateSize = () => {
       setWindowSize(window.innerWidth);
     };
-    window.addEventListener('resize', updateSize);
-    return () => window.removeEventListener('resize', updateSize);
+    window.addEventListener("resize", updateSize);
+    return () => window.removeEventListener("resize", updateSize);
   }, []);
 
   return windowSize;
